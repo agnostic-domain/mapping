d4f22741195029c976b4ef00b8557b0ac245c61b
Daniel Iwaniec <me@danieliwaniec.com>
2020-03-29 19:16:06 +0200

    tests

--- test/Unit/Helper.php
+++ test/Unit/Helper.php
@@ -19,12 +19,12 @@ final class Helper extends TestCase
         $this->assertSame($data, (new ADM\Helper())->data($entity));
     }
 
-    public function testRetrievingData(): void
+    public function testCreatingCollectionHelper(): void
     {
         $data = new stdClass();
         $entity = new stdClass();
-        ADM\Helper::$data[spl_object_id($entity)] = $data;
+        $collectionHandler = (new ADM\Helper())->collection([1 => $data], [1 => $entity]);
 
-        $this->assertSame($data, (new ADM\Helper())->data($entity));
+        $this->assertInstanceOf(ADM\Helper\CollectionHandler::class, $collectionHandler);
     }
 }
--- test/Unit/Helper/CollectionHandler.php
+++ test/Unit/Helper/CollectionHandler.php
@@ -4,12 +4,34 @@ declare(strict_types=1);
 
 namespace ADM\Test\Unit\Helper;
 
+use ADM;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 final class CollectionHandler extends TestCase
 {
-    public function test(): void
+    public function testRemoved(): void
     {
         $this->assertTrue(true);
+    }
+
+    public function testAdded(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testChanged(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testOtherCall(): void
+    {
+        $data = new stdClass();
+        $entity = new stdClass();
+        ADM\Helper::$data[spl_object_id($entity)] = $data;
+
+        $collectionHandler = new ADM\Helper\CollectionHandler(new ADM\Helper(), [], []);
+        $this->assertSame($data, $collectionHandler->data($entity));
     }
 }
