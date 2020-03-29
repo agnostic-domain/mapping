29d887118897150c60568b67ba9a322ac62cbceb
Daniel Iwaniec <me@danieliwaniec.com>
2020-03-29 18:52:32 +0200

    hydrator test

--- test/Unit/Hydrator.php
+++ test/Unit/Hydrator.php
@@ -8,8 +8,30 @@ use PHPUnit\Framework\TestCase;
 
 final class Hydrator extends TestCase
 {
-    public function test(): void
+    public function testHydrating(): void
     {
-        $this->assertTrue(true);
+        $hydrator = new \ADM\Hydrator(Hydratee::class);
+        $hydrator->integer(1);
+        $hydrator->string(function() { return 'test'; });
+        $object = $hydrator();
+
+        $this->assertSame(1, $object->getInteger());
+        $this->assertSame('test', $object->getString());
+    }
+}
+
+class Hydratee
+{
+    private int $integer;
+    private string $string;
+
+    public function getInteger(): int
+    {
+        return $this->integer;
+    }
+
+    public function getString(): string
+    {
+        return $this->string;
     }
 }
