be053c4032f623e1bda4b4b8b92e1068386c6e03
Daniel Iwaniec <me@danieliwaniec.com>
2020-03-29 17:57:50 +0200

    test scaffold

---
+++ test/Unit/Helper.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADM\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class Helper extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ test/Unit/Helper/CollectionHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADM\Test\Unit\Helper;
+
+use PHPUnit\Framework\TestCase;
+
+final class CollectionHandler extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
